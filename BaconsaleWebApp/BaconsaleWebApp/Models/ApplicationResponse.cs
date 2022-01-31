3e2e4194518da01ce9e8e5857e6afb74e1710d0e
Csbennett22 <90526059+Csbennett22@users.noreply.github.com>
2022-01-31 10:30:42 -0700

    Mission4

--- BaconsaleWebApp/BaconsaleWebApp/Models/ApplicationResponse.cs
+++ BaconsaleWebApp/BaconsaleWebApp/Models/ApplicationResponse.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
 
 namespace BaconsaleWebApp.Models
 {
